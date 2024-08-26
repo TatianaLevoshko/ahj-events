ae59bfdd1451b52e4e9569b2d5dde71339ab3e7d
Tatyana Lev <delovoiy86@gmail.com>
2024-08-26 03:55:29 +0300

    test

--- src/js/startGame.js
+++ src/js/startGame.js
@@ -1,8 +1,10 @@
 import Game from "./Game";
 
-export default class Start {
+class Start {
   static init() {
     this.game = new Game("#game-board");
     this.game.startGame();
   }
 }
+
+export default Start;
